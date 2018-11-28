38cd6fda190de3c045b85d384761733e151f7b50
Chunhui Liu <chunhuiliu960@gmail.com>
2018-11-27 21:03:37 -0500

    Update util.py
    re-write load_all function using interpolation

--- src/util.py
+++ src/util.py
@@ -1,5 +1,6 @@
 import numpy as np 
 import os
+
 def load_data(name):
     lines = open(name).readlines()
     lines = [[float(x) for x in line.strip().split(' ')] \
@@ -18,9 +19,13 @@ def load_all(datafolder='./data/'):
         filename = os.path.join(datafolder, name)
         class_id = int(filename.split('_')[0].split('a')[-1])
         subject_id = int(filename.split('_')[1].split('s')[-1])
-        data = load_data(filename)
-        zeros = np.zeros([max_length - data.shape[0], 20, 4])
-        data = np.concatenate([data, zeros], axis=0)
+        old_data = load_data(filename)
+        data = np.zeros([max_length, 20, 4])
+        for x in range(20):
+            for y in range(4):
+                data[:,x,y] = np.interp(np.arange(max_length), np.arange(old_data.shape[0]), old_data[:,x,y])
+        # zeros = np.zeros([max_length - data.shape[0], 20, 4])
+        # data = np.concatenate([data, zeros], axis=0)
         all_data.append(data)
         all_class.append(class_id)
         all_subject.append(subject_id)
