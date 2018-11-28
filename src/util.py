0ff7137312eb68c9bf49f939f4fa1829a31eff08
Chunhui Liu <chunhuiliu960@gmail.com>
2018-11-27 21:21:09 -0500

    Update util.py
    add function load_one

--- src/util.py
+++ src/util.py
@@ -9,8 +9,8 @@ def load_data(name):
     # print(lines.shape) # frames x 20 x 4
     return lines
 
-def load_all(datafolder='./data/'):
-    #  items_num x frames x 20 x 4
+def load_all(datafolder='../data/'):
+    #  video_number x frames x 20 x 4
     all_data = []
     all_class = []
     all_subject = []
@@ -24,11 +24,22 @@ def load_all(datafolder='./data/'):
         for x in range(20):
             for y in range(4):
                 data[:,x,y] = np.interp(np.arange(max_length), np.arange(old_data.shape[0]), old_data[:,x,y])
-        # zeros = np.zeros([max_length - data.shape[0], 20, 4])
-        # data = np.concatenate([data, zeros], axis=0)
         all_data.append(data)
         all_class.append(class_id)
         all_subject.append(subject_id)
     return np.stack(all_data), np.stack(all_class), np.stack(all_subject)
 
+def load_one(datafolder='../data/', label=1, subject=1, instant=1):
+    name = "a%02d_s%02d_e%02d_skeleton3D.txt"%(label, subject, instant)
+    filename = os.path.join(datafolder, name)
+    old_data = load_data(filename)
+    max_length = 100
+    data = np.zeros([max_length, 20, 4])
+    for x in range(20):
+        for y in range(4):
+            data[:,x,y] = np.interp(np.arange(max_length), np.arange(old_data.shape[0]), old_data[:,x,y])
+    return np.reshape(data, (1, data.shape[0], data.shape[1], data.shape[2]))
+
+def plot_one(one_video):
+    pass
 
