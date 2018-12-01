2d8ad4c540e3c413fa75303a0cd0325c08b88193
kenan <dknyxh@gmail.com>
2018-12-01 16:28:33 -0500

    Add animation

--- src/util.py
+++ src/util.py
@@ -1,6 +1,10 @@
 import numpy as np 
 import os
 
+import matplotlib.pyplot as plt
+import matplotlib.animation
+from mpl_toolkits.mplot3d import Axes3D
+
 def load_data(name):
     lines = open(name).readlines()
     lines = [[float(x) for x in line.strip().split(' ')] \
@@ -39,3 +43,26 @@ def load_one(datafolder='../data/', label=1, subject=1, instant=1):
         for y in range(4):
             data[:,x,y] = np.interp(np.linspace(0, old_data.shape[0]-1, max_length), np.arange(old_data.shape[0]), old_data[:,x,y])
     return np.reshape(data, (1, data.shape[0], data.shape[1], data.shape[2]))
+
+def animate_skeleton(data_sequence):
+    """
+    data_sequence: a Frame x 20 x 4
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    title = ax.set_title('3D Test')
+    def update_graph(num):
+        current_joint = data_sequence[num,:,:]
+        graph.set_data (current_joint[:,0], current_joint[:,1])
+        graph.set_3d_properties(current_joint[:,2])
+        title.set_text('3D Test, time={}'.format(num))
+        return title, graph
+    graph, = ax.plot(data_sequence[0,:,0], data_sequence[0,:,1], data_sequence[0,:,2], linestyle="", marker="o")
+    ani = matplotlib.animation.FuncAnimation(fig, update_graph, 100, 
+                               interval=40, blit=True)
+
+    plt.show()
+
+if __name__ == '__main__':
+    data = load_one()
+    animate_skeleton(np.squeeze(data))
