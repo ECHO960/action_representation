c0550e9e8f207501c0e92839890e4149453fc8b1
kenan <dknyxh@gmail.com>
2018-12-14 11:08:58 -0500

    Add some helper function for plotting

--- src/approximation.py
+++ src/approximation.py
@@ -39,8 +39,8 @@ def least_sqaure_approximation(x, y, repeat, basis, order, period = 2):
     coeff = np.linalg.lstsq(stack_A, y)[0]
 
     error = stack_A.dot(coeff) - y
-    # print("L_inf:",np.max(error))
-    # print("L_lsq:", np.sqrt(np.sum(error ** 2)))
+    print("L_inf:",np.max(error))
+    print("L_lsq:", np.sqrt(np.sum(error ** 2)))
     return error, coeff, A
 
 def do_approximation(data):
@@ -60,12 +60,89 @@ def do_approximation(data):
     general_curve = relative_time_transform_rev(np.reshape(general_curve, shape))[0]
     return general_curve, final_coeff
 
-if __name__ == '__main__':
+def draw_approximation_curve_naive():
+    """
+    sequence - 1 x 
+    """
+    
     all_data = []
-    data, classes, subjects = util.load_all()
-    all_data = data[np.where(classes == 1)]
-    general_curve, final_coeff = do_approximation(all_data)
-    util.animate_skeleton(general_curve)
+    for i in [1,2,3,5,6,7,8,9,10]:
+        for j in range(1,4):
+            data = util.load_one('../data/', 1, i, j)
+            all_data.append(data)
+    y = np.squeeze(np.array(all_data)) #sample x frame x 20 x coordinate
+    joint = 8
+    coordinate = 2      
+    coordinate_str = 'xyz'
+    import matplotlib.pyplot as plt
+    x = np.arange(y.shape[1])
+    # plt.plot(basis.dot(coeff), c='r')
+
+    y_flat = y[:,:,joint,coordinate].reshape(-1) #action x frame
+    error, coeff, basis = least_sqaure_approximation(x, y_flat, y.shape[0], 'poly', 10)
+    general_curve = basis.dot(coeff)
+
+    for i in range(27):
+        if i == 0:
+            plt.plot(x, y[i,:, joint, coordinate],c='b', label='Sample curves')
+        else:
+            plt.plot(x, y[i,:, joint, coordinate],c='b')
+    plt.grid(True)
+    plt.plot(x, general_curve, c='r',linewidth='5', label='Approximated curve')
+    plt.legend()
+    plt.title("Naive method: Trajectory of joint %d coordinate %s" % (joint, coordinate_str[coordinate]))
+    plt.xlabel('Frame')
+    plt.ylabel('Position')
+    plt.show()
+
+def draw_approximation_curve_temporal_difference(joint):
+    """
+    sequence - 1 x 
+    """
+    
+    all_data = []
+    for i in [1,2,3,5,6,7,8,9,10]:
+        for j in range(1,4):
+            data = util.load_one('../data/', 1, i, j)
+            all_data.append(data)
+    y = np.squeeze(np.array(all_data)) #sample x frame x 20 x coordinate
+    y = relative_time_transform(y)[:,1:,:,:]
+    coordinate = 2
+    coordinate_str = 'xyz'
+    import matplotlib.pyplot as plt
+    x = np.arange(y.shape[1])
+    # plt.plot(basis.dot(coeff), c='r')
+
+    y_flat = y[:,:,joint,coordinate].reshape(-1) #action x frame
+    error, coeff, basis = least_sqaure_approximation(x, y_flat, y.shape[0], 'poly', 10)
+    general_curve = basis.dot(coeff)
+
+    for i in range(27):
+        if i == 0:
+            plt.plot(x, y[i,:, joint, coordinate],c='b', label='Sample curves')
+        else:
+            plt.plot(x, y[i,:, joint, coordinate],c='b')
+    plt.grid(True)
+    plt.plot(x, general_curve, c='r',linewidth='5', label='Approximated curve')
+    plt.legend()
+    plt.title("Temporal difference method: Trajectory of joint %d coordinate %s" % (joint, coordinate_str[coordinate]))
+    plt.xlabel('Frame')
+    plt.ylabel('Position')
+    plt.show()
+
+if __name__ == '__main__':
+    # all_data = []
+    # data, classes, subjects = util.load_all()
+    # for i in range(1,21):
+    #     print("Class : ",i)
+    #     all_data = data[np.where(classes == i)]
+    #     print(np.isnan(all_data).any())
+    #     # general_curve, final_coeff = do_approximation(all_data)
+    #     print("============Class : ",i)
+    # util.animate_skeleton(general_curve)
+    draw_approximation_curve_naive()
+    # for i in range(20):
+    #     draw_approximation_curve_temporal_difference(i)
 
     # # import matplotlib.pyplot as plt
     # # plot_x = np.arange(data.shape[1], 0.01)
